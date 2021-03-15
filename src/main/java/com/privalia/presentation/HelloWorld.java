357c00af7ebcd3f6cddad0e4e505bfed0ec9f11f
jordiascension <jordiascension@enterpriseconsulting.es>
2021-03-15 19:58:15 +0100

    First commit with dependency injection and xml configuration file

---
+++ src/main/java/com/privalia/presentation/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.privalia.presentation;
+
+public class HelloWorld {
+
+	private String hello;
+
+	/**
+	 * @return the hello
+	 */
+	public String getHello() {
+		return hello;
+	}
+
+	/**
+	 * @param hello the hello to set
+	 */
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
+	
+	public HelloWorld()
+	{
+		
+	}
+	
+	public HelloWorld(String hello)
+	{
+		this.hello = hello;
+	}
+	
+}
